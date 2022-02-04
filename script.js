8ad86254130adbc1bdbcbbd43e60be5afd18a8de
boyetech111 <97601467+boyetech111@users.noreply.github.com>
2022-02-04 12:12:32 +0100

    working login and register

---
+++ script.js
@@ -0,0 +1,11 @@
+// const body = document.querySelector("body"),
+//       sidebar = document.querySelector("sidebar"),
+//       toggle = document.querySelector("toggle"),
+//       searchBtn = document.querySelector(".search-box"),
+//       modeSwitch = document.querySelector(".toggle-switch"),
+//       modetext = document.querySelector(".toggle-switch");
+
+//       modeSwitch.addEventListener("click", () =>{
+//           body.classList.add("darkMode");
+//           console.log(body.classList);
+//       });
