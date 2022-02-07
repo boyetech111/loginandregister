343fc5074441f758d2d24b2bb8278609212af0ea
boyetech111 <97601467+boyetech111@users.noreply.github.com>
2022-02-07 14:34:11 +0100

    Edited sript.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// const body = document.querySelector("body"),
-//       sidebar = document.querySelector("sidebar"),
-//       toggle = document.querySelector("toggle"),
-//       searchBtn = document.querySelector(".search-box"),
-//       modeSwitch = document.querySelector(".toggle-switch"),
-//       modetext = document.querySelector(".toggle-switch");
+const body = document.querySelector("body"),
+      sidebar = document.querySelector("sidebar"),
+      toggle = document.querySelector("toggle"),
+      searchBtn = document.querySelector(".search-box"),
+      modeSwitch = document.querySelector(".toggle-switch"),
+      modetext = document.querySelector(".toggle-switch");
 
-//       modeSwitch.addEventListener("click", () =>{
-//           body.classList.add("darkMode");
-//           console.log(body.classList);
-//       });
+      modeSwitch.addEventListener("click", () =>{
+          body.classList.add("darkMode");
+          console.log(body.classList);
+      });
